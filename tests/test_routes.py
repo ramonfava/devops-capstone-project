b07d27cebcde4971ad983d0fd40624f82f5e483c
Ramon Fava <ramonfava@gmail.com>
2023-07-10 13:44:17 -0400

    add list accounts feature

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -138,3 +138,16 @@ class TestAccountService(TestCase):
         """It should not Read an Account that is not found"""
         response = self.client.get(f'{BASE_URL}/0')
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_get_account_list(self):
+        """It should Get a list of Accounts"""
+        self._create_accounts(5)
+        response = self.client.get(BASE_URL)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        data = response.get_json()
+        self.assertEqual(len(data), 5)
+
+    def test_get_account_not_found(self):
+        """It should not Read an Account that is not found"""
+        response = self.client.get(f'{BASE_URL}/0')
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
